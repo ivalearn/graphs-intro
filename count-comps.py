0b7a5a70aad6a5c901c4ac8b7a2454630f8c7841
Ivan Andreev <ivandeex@gmail.com>
2017-10-27 05:52:27 +0300

    improve the component counting algorithm

--- count-comps.py
+++ count-comps.py
@@ -1,25 +1,23 @@
-nv, ne = (int(x) for x in input().split())
+nv, ne = map(int, input().split())
 
-verts = [set() for _ in range(nv)]
+verts = [[] for _ in range(nv)]
 for _ in range(ne):
-    a,b = (int(x) for x in input().split())
-    verts[a-1].add(b-1)
-    verts[b-1].add(a-1)
+    a, b = map(int, input().split())
+    verts[a-1].append(b-1)
+    verts[b-1].append(a-1)
 
+visited = [False] * nv
+nc = 0
+        
 def dfs(v):
     visited[v] = True
     for w in verts[v]:
         if not visited[w]:
             dfs(w)
 
-visited = [False] * nv
-nc = 0
-while 1:
-    try:
-        i = visited.index(False)
-    except ValueError:
-        break
-    nc += 1
-    dfs(i)
-
+for v in range(len(visited)):
+    if not visited[v]:
+        nc += 1
+        dfs(v)
+   
 print(nc)
