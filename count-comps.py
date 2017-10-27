1fde55493053fc5c2133b1899f547051d69bb014
Ivan Andreev <ivandeex@gmail.com>
2017-10-28 00:57:09 +0300

    use numpy arrays to count connected components

--- count-comps.py
+++ count-comps.py
@@ -1,23 +1,23 @@
 nv, ne = map(int, input().split())
-
-verts = [[] for _ in range(nv)]
+adj = [[] for _ in range(nv)]
 for _ in range(ne):
     a, b = map(int, input().split())
-    verts[a-1].append(b-1)
-    verts[b-1].append(a-1)
-
+    adj[a-1].append(b-1)
+    adj[b-1].append(a-1)
 visited = [False] * nv
+stack = [None] * nv
 nc = 0
-        
-def dfs(v):
-    visited[v] = True
-    for w in verts[v]:
-        if not visited[w]:
-            dfs(w)
-
-for v in range(len(visited)):
-    if not visited[v]:
+for i in range(nv):
+    if not visited[i]:
         nc += 1
-        dfs(v)
-   
+        stack[0] = i
+        ns = 1
+        while ns:
+            ns -= 1
+            v = stack[ns]
+            for w in adj[v]:
+                if not visited[w]:
+                    visited[w] = True
+                    stack[ns] = w
+                    ns += 1
 print(nc)
